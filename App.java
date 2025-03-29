18daafb6c2952f421e0bf3bd99243fd1c82c825a
KasthooriVijayarasa <selvivijay0904@gmail.com>
2025-03-29 19:52:41 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,23 @@
+import P1.*;
+import P2.*;
+import P1.P3.*;
+
+class App{
+	
+	public static void main(String[] args){
+		
+		MyClass mc=new MyClass();
+		mc.display();
+		
+		Class2 c1=new Class2();
+		c1.display();
+		
+		Class3 c3=new Class3();
+		c3.display();
+		
+		Class4 c4=new Class4();
+		c4.display();
+		
+	}
+	
+}
