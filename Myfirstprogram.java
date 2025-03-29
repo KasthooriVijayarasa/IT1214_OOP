7b456c323adad301c4c8a1ef70149505be6936bc
KasthooriVijayarasa <selvivijay0904@gmail.com>
2025-03-29 10:59:18 +0530

    Add files via upload

---
+++ Myfirstprogram.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Myfirstprogram{
+	
+		public static void main(String[] args){
+			
+			System.out.print("Hi");
+			System.out.print("This is IT 1214 Practical Session 1");
+			System.out.println("This prints next in a new line");
+			System.out.println("This is in a new line");
+			
+		}
+		
+}
