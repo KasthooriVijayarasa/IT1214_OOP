f7d5ed5ada1b84702f8b53a564c0e3f233fdc1df
KasthooriVijayarasa <selvivijay0904@gmail.com>
2025-04-13 12:54:35 +0530

    Add files via upload

---
+++ ex.java
@@ -0,0 +1,34 @@
+/*write a java program to create a Car class with private field Model.
+provide a getter and setter method for the model.
+In the main vlass, Create an object of Car, set the model name to "Toyota Corolla"
+and print it.*/
+
+class Car
+{
+
+private String model;
+
+//Getter
+public String getModel()
+{
+	return model;
+}
+
+//Setter
+public void setModel(String newModel)
+{
+	this.model=newModel;
+}
+
+}
+
+public class ex
+{
+	public static void main(String args[])
+	{
+		Car c1=new Car();
+		c1.setModel("Toyota Corolla");
+		System.out.println(c1.getModel());
+	}
+}
+
