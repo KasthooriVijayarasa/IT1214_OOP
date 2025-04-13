32f863392e0a48e0d9ab5f55470c946c0d82e2d9
KasthooriVijayarasa <selvivijay0904@gmail.com>
2025-04-13 12:46:32 +0530

    Add files via upload

---
+++ Invoice/App.java
@@ -0,0 +1,9 @@
+import Human.* ;
+
+class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Invoice App");
+		Person kasthoori = new Person ("Kasthoori") ;
+		kasthoori.talk("Hello from Kasthoori") ;
+	}
+}
