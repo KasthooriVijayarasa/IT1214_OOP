a45fb68344b5b116a1de55bf9782807b0d769d8c
KasthooriVijayarasa <selvivijay0904@gmail.com>
2025-04-13 12:42:27 +0530

    Add files via upload

---
+++ App1.java
@@ -0,0 +1,16 @@
+import it.*;
+import it.hons.*;
+import amc.*;	
+class App1{
+	public static void main(String[] ar){
+		amc.FirstYear fya=new amc.FirstYear();
+		it.FirstYear fyi=new it.FirstYear();
+		SecondYear sy=new SecondYear();
+		FourthYear ithons=new FourthYear();
+		
+		fya.display();
+		fyi.display();
+		sy.display();
+		ithons.display();
+	}
+}
